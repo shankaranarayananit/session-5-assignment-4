4ad1f77e5f7609aa46fd519cb3adc4f5685804bc
shankaranarayananit <shankara.88it@gmail.com>
2016-11-02 01:37:07 +0530

    Add files via upload

---
+++ assignment 4.js
@@ -0,0 +1,53 @@
+﻿var employee = new Array();
+ 
+employee[0] = {
+    Name: "Aru",
+    Age: 25,
+    salary: 20000,
+    address: {
+        city: "chennai",
+        state: "Tamilnadu",
+        Pincode: 600001
+    }
+};
+employee[1] = {
+    Name: "Balaji",
+    Age: 23,
+    salary: 26000,
+    address: {
+        city: "chennai",
+        state: "Tamilnadu",
+        Pincode: 600002
+    }
+};
+employee[2] = {
+    Name: "celine",
+    Age: 26,
+    salary: 25000,
+    address: {
+        city: "chennai",
+        state: "Tamilnadu",
+        Pincode: 600003
+    }
+};
+employee[3] = {
+    Name: "Don",
+    Age: 28,
+    salary: 28000,
+    address: {
+        city: "chennai",
+        state: "Tamilnadu",
+        Pincode: 600004
+    }
+};
+employee[4] = {
+    Name: "Elimalai",
+    Age: 25,
+    salary: 26000,
+    address: {
+        city: "chennai",
+        state: "Tamilnadu",
+        Pincode: 600010
+    }
+};
+console.log(employee[3]);
